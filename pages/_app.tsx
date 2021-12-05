e0ba1c53527ab74860ecca827f2861c6c5b71b5d
DESKTOP-IF0P0KI\linsy <yana_586@mail.ru>
2021-12-05 20:35:11 +0300

    update main config files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import {AppProps} from "next/dist/shared/lib/router/router";
+import '../styles/globals.css';
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <>
+    <Head>
+      <title>MyTop is the best app</title>
+      <meta name="description" content="MyTop"/>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+    <Component {...pageProps} />;
+  </>;
 }
 
-export default MyApp
+export default MyApp;
